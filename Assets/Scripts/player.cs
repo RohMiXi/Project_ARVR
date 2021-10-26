30589a8d79fcf21ad8f3d064ccd934392ae65d87
Roh Jae Yeal <rjy0924@gmail.com>
2021-10-26 15:53:54 +0900

    test
    test

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 5.0f;
+    public float jumpforce = 5.0f;
+    public bool isOnGround = true;
+    private float horizontalInput;
+    private float forwardInput;
+    private Rigidbody playerRb;
+
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true; 
+        }
+    }
+}
