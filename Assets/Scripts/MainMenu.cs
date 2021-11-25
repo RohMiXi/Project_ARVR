927369e40c36c7f64cacf56735c972a70becebc4
Junhyeong Jo <whwnsgud13@hanyang.ac.kr>
2021-11-25 15:56:02 +0900

    Array_Button interaction

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,15 +6,23 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject[] _myPanel;
+    public GameObject[] _myPanel; // 배열로 받아서 입력값을 줌. 
+    //public GameObject[] _closePanel;
     public GameObject Information;
-    public void OpenPanel(int num)
+    public void OpenPanel(int num) // 배열에 들어갈 변수 값을 입력 
     {
         //_myPanel = GameObject.FindGameObjectWithTag("Information");
-        if(_myPanel != null)
-        {
-            _myPanel[num].SetActive(true);
+        for(int i = 0; i < _myPanel.Length ; i++){ //_myPanel.Length를 넣어준 이유는, text 문자 수의 값을 할당해서 열리도록 하기 위해서
+            _myPanel[i].SetActive(false); 
         }
+        _myPanel[num].SetActive(true);
     }
+    // public void ClosePanel(int num)
+    // {
+    //     if(_closePanel != null)
+    //     {
+    //         _closePanel[num].SetActive(true);
+    //     }
+    // }
 }
  
