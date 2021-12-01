2f25bc29c6241e484f05e725e498f51da1c7b7f2
Junhyeong Jo <whwnsgud13@hanyang.ac.kr>
2021-12-01 09:30:19 +0900

    LineAnimator

--- Assets/Scripts/LineCreater.cs
+++ Assets/Scripts/LineCreater.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class LineCreater : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float animationDuration = 5f;
+    public LineRenderer lineRenderer;
+    private void Start(){
+        lineRenderer = GetComponent<LineRenderer>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        StartCoroutine(AnimateLine());
     }
+    public IEnumerator AnimateLine()
+    {
+        float startTime = Time.time;
+        Vector3 startPosition = lineRenderer.GetPosition(0);
+        Vector3 endPosition = lineRenderer.GetPosition(1);
+        Vector3 pos = startPosition;
+        while(pos != endPosition){
+            float t = (Time.time - startTime) / animationDuration;
+            pos = Vector3.Lerp(startPosition, endPosition, t);
+            lineRenderer.SetPosition(1, pos);
+            yield return null;
+        }
+    }
+    // Start is called before the first frame update
+
 }
