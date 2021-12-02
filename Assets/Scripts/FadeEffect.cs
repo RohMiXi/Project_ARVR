c0329a1176be8cd90fac910773209c3b3d20c90f
Junhyeong Jo <whwnsgud13@hanyang.ac.kr>
2021-12-02 14:12:09 +0900

    solution_fadeout Courtine

--- Assets/Scripts/FadeEffect.cs
+++ Assets/Scripts/FadeEffect.cs
@@ -17,19 +17,21 @@ public class FadeEffect : MonoBehaviour
     // 알파 a값이 0보다 크면 알파값이 감소.
     private IEnumerator Fade(float start, float end) 
     {
-        float currnetTIme = 0.0f;
+        float currentTime = 0.0f;
         float percent = 0.0f;
 
+        while(percent < 1){
         // fadeTime으로 나누어서 fadeTime 시간 동안 
         // precent 값이 0에서 1로 증가하도록 함. 
-        currnetTIme += Time.deltaTime;
-        percent =  currnetTIme / fadeTime;
+        currentTime += Time.deltaTime;
+        percent =  currentTime / fadeTime;
 
         // 알파값을 start부터 end까지 fadeTime 시간 동안 변화시킴. 
         Color color = image.color;
-        color.a = Mathf.Lerp =(start, end, percent);
+        color.a = Mathf.Lerp(start, end, percent);
         image.color = color;
 
         yield return null;
+        }
     }
 }   
