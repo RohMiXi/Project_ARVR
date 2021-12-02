8b9ebc45b15b565f5c024eabcb7ca537de2172a9
Junhyeong Jo <whwnsgud13@hanyang.ac.kr>
2021-12-02 11:58:53 +0900

    Fixed Data 1

--- Assets/Ballmovement.cs
+++ Assets/Ballmovement.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Ballmovement : MonoBehaviour
 {
+    // public GameObject[] waypoint;
     public Transform target;
-    //ublic float t;
+    //public float t;
     public float speed;
+    // int current = 0; // 현재 오브젝트가 있는 위치. 
+    // float WPradius = 1;
 
 
     // Update is called once per frame
@@ -15,5 +18,16 @@ public class Ballmovement : MonoBehaviour
         Vector3 a = transform.position;
         Vector3 b = target.position;
         transform.position = Vector3.MoveTowards(a,b, speed);
+
+        // if(Vector3.Distance(waypoint[current].transform.position, transform.position) < WPradius)
+        // {
+        //     current++;
+        //     if(current >= waypoint.Length)
+        //     {
+        //         current = 0;
+        //     }
+        // }
+        // transform.position = Vector3.MoveTowards(transform.position, waypoint[current].transform.position, Time.deltaTime * speed);
+
     }
 }
