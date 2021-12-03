15a3457e0fe68ab2578223dcb11cc3fe94935484
Junhyeong Jo <whwnsgud13@hanyang.ac.kr>
2021-12-04 04:07:06 +0900

    json 다시 해본 결과 똑같음

--- Assets/Player_Controller.cs
+++ Assets/Player_Controller.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-public class playerInformation : MonoBehaviour
+public class Player_Controller : MonoBehaviour
 {
     public PlayerData playerData;
 
