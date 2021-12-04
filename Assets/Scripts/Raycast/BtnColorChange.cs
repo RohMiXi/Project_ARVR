5391277ccd72a8151a968b98dcab70f06828ad21
Roh Jae Yeal <rjy0924@gmail.com>
2021-12-05 01:57:01 +0900

    test
    test

---
+++ Assets/Scripts/Raycast/BtnColorChange.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+    public class BtnColorChange : MonoBehaviour
+{
+    public bool hoverStatus;
+    public GameObject Button;
+    Renderer btnMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(hoverStatus);
+        btnMat = Button.GetComponent<Renderer>();
+        if (hoverStatus)
+        {
+            btnMat.material.SetColor("_EmissionColor", Color.white * 2.5f);
+        }
+    }
+
+    public virtual bool isHoverActive
+    {
+        get => hoverStatus;
+        set => hoverStatus = true;
+    }
+} 
+
+   
+
