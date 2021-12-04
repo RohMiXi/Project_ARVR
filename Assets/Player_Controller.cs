9496cdabbb8caab0aed9c1d8c1b3ad2224b4d8b8
Junhyeong Jo <whwnsgud13@hanyang.ac.kr>
2021-12-04 17:04:46 +0900

    컨트롤러 값 수정중

--- Assets/Player_Controller.cs
+++ Assets/Player_Controller.cs
@@ -1,5 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+// using System.Collections;
+// using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using UnityEngine.UI;
@@ -7,40 +7,56 @@ using UnityEngine.UI;
 
 public class Player_Controller : MonoBehaviour
 {
-    public PlayerData playerData;
+    public InputField NameInput;
 
-    
-    [ContextMenu("To Json Data")]
-    void SavePlayerDataToJson()
+
+    public void SaveToJson()
     {
-        string jsonData = JsonUtility.ToJson(playerData, true);
-        string path = Path.Combine(Application.dataPath, "PlayerData.json");
-        File.WriteAllText(path, jsonData);
-    }
-    // [ContextMenu("From Json Data")]
-    // void LoadPlayerDataToJson()
-    // {
-    //     string path = Path.Combine(Application.dataPath, "playerData.json");
-    // }
-    public GameObject Panel;
-    public void OpenPaenl()
-    {
-        if(Panel != null)
-        {
-            bool isActive = Panel.activeSelf;
-            Panel.SetActive(!isActive);
-        }
+        Player_Controller data = new Player_Controller();
+        data.NameInput = NameInput.text;
+
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(Application.dataPath+"PlayerDataFile.Json", json);
     }
 
+    public void LoadFromJson()
+    {
+        string json = File.ReadAllText(Application.dataPath + "/PlayerDataFile.json");
+        Player_Controller data = JsonUtility.FromJson<Player_Controller>(Json);
 
+        NameInput.text = data.NameInput;   
+    }
+//     public PlayerData playerData;
 
-[System.Serializable]
-public class PlayerData
-{
-    public string name;
-    public int age;
-    public int level;
-    public bool isDeed;
-    public string[] items;
-}
-}
+//     [ContextMenu("To Json Data")]
+//     void SavePlayerDataToJson()
+//     {
+//         string jsonData = JsonUtility.ToJson(playerData, true);
+//         string path = Path.Combine(Application.dataPath, "PlayerData.json");
+//         File.WriteAllText(path, jsonData);
+//     }
+//     // [ContextMenu("From Json Data")]
+//     // void LoadPlayerDataToJson()
+//     // {
+//     //     string path = Path.Combine(Application.dataPath, "playerData.json");
+//     // }
+//     public GameObject Panel;
+//     public void OpenPaenl()
+//     {
+//         if(Panel != null)
+//         {
+//             bool isActive = Panel.activeSelf;
+//             Panel.SetActive(!isActive);
+//         }
+//     }
+
+//     [System.Serializable]
+//     public class PlayerData
+//     {
+//         public string name;
+//         public int age;
+//         public int level;
+//         public bool isDeed;
+//         public string[] items;
+//     }
+// }
