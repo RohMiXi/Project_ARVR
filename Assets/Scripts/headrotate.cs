f5895871f5757acd527e22426ef4618d680c3a35
kimdoone <92706211+kimdoone@users.noreply.github.com>
2021-12-11 00:30:35 +0900

    .
    .

---
+++ Assets/Scripts/headrotate.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class headrotate : MonoBehaviour
+{
+    public XRController rightcontroller = null;
+    private CharacterController character;
+    private GameObject _camera;
+    private float count = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        _camera = GetComponent<XRRig>().cameraGameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rightcontroller.inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 pos))
+        {
+
+            float angle = Mathf.Atan2(pos.x, pos.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+        }
+    }
+}
